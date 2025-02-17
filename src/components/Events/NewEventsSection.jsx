123deb86604c23b9ad253f44061636fd7e2ec2a0
Saul Pereira <saul_pereira@hotmail.com>
2025-02-17 17:25:55 -0600

    411. Understanding & Configuring Query Behaviors - Cache & Stale Data
    411. Understanding & Configuring Query Behaviors - Cache & Stale Data

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -9,6 +9,8 @@ export default function NewEventsSection() {
   const { data, isPending, isError, error, refetch } = useQuery({
     queryKey: ["events"],
     queryFn: fetchEvents,
+    staleTime: 5000,
+    // gcTime: 1000,
   });
 
   let content;
